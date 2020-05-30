e6295560441a1caf685761794e1cc5c428ee24c9
Tyler Philbrick <t5j6p9@gmail.com>
2020-05-30 12:43:49 -0700

    lmain: add data symbols to the executable

--- lmain.c
+++ lmain.c
@@ -1,11 +1,16 @@
 
 #include <lib.h>
 
+int bss_symbol;
+int data_symbol = 100;
+
 int lmain() {
     lprint("Hello World\n");
     lprint("Let's make sure it doesn't resolve a second time\n");
     inc_global();
     ulong c = some_global;
     ulong *d = &some_global;
+    bss_symbol = (unsigned long)&c;
+    data_symbol = (unsigned long)d;
     return 0;
 }
